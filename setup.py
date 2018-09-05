fd9ce3d07e4ddb00c3b244299385a83fdc458e85
kireet <kireet@feedly.com>
2018-09-04 17:35:50 -0700

    include license file, bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/feedly/python-api-client'
 EMAIL = 'kireet@feedly.com'
 AUTHOR = 'Kireet'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = 0.1
+VERSION = 0.13
 
 # What packages are required for this module to be executed?
 #REQUIRED = [
